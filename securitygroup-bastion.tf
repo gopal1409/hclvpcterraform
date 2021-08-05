7f27e9d2a4b5a28034d72364eeca99dee6faec05
Gopal <Gopal1409@gmail.com>
2021-08-05 08:10:52 -0700

    security group

--- securitygroup-bastion.tf
+++ securitygroup-bastion.tf
@@ -1,6 +1,6 @@
 module "public_bastion_sg"{
     source = "terraform-aws-modules/security-group/aws"
-    version = 3.18.0
+    version = "3.18.0"
     name = "public-bastion-sg"
     vpc_id = module.vpc.vpc_id
     ingress_rules = ["ssh-tcp"]
