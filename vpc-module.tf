a0c4ffacc641f2337c169983992c5c4c0e80de9b
Gopal <Gopal1409@gmail.com>
2021-08-05 07:40:09 -0700

    vpc with nat gw

--- vpc-module.tf
+++ vpc-module.tf
@@ -8,6 +8,15 @@ module "vpc" {
   azs             = var.vpc_availability_zones
   private_subnets = var.vpc_private_subnet
   public_subnets  = var.vpc_public_subnet
+#database subnet
+database_subnet = var.vpc_database_subnet
+create_database_subnet_group = var.vpc_create_database_subnet_group
+create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+#deploy intennet gw
+#create_database_internet_gateway = true
+#enable natgw outbound connection
+enable_nat_gateway = var.vpc_enable_nat_gateway
+single_nat_gateway = var.vpc_single_nat_gateway
 
   #VPC DNS Parameters
   
--- vpc-varaibles.tf
+++ vpc-varaibles.tf
@@ -29,7 +29,26 @@ variable "vpc_database_subnet" {
   type = list(string)
   default = ["10.0.151.0/24","10.0.152.0/24"]
 }
+variable "vpc_create_database_subnet_group" {
+  type = bool
+  default = true
+}
+#database subnet route table
+variable "vpc_create_database_subnet_route_table" {
 
+  type = bool
+  default = true
+}
+
+#vpc enable nat gateway
+variable "vpc_enable_nat_gateway" {
+  type = bool
+  default = true
+}
+variable "vpc_single_nat_gateway" {
+  type = bool
+  default = true
+}
 
 
 
