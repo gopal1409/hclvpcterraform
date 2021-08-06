f5178764dd092c7739e4017b407d34e1e10da6ba
Gopal <Gopal1409@gmail.com>
2021-08-06 06:48:21 -0700

    alb

--- datasource_route53.tf
+++ datasource_route53.tf
@@ -1,3 +1,9 @@
 data "aws_route53_zone" "mydomain"{
     name = "devopsincloud.com"
 }
+output "mydomain_zoneid" {
+    value = data.aws_route53_zone.mydomain.zone_id
+}
+output "mydomain_name"{
+    value = data.aws_route53_zone.mydomain.name
+}
