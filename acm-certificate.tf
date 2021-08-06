f8896ac1c8f246c9494ac071f5322271f155748e
Gopal <Gopal1409@gmail.com>
2021-08-06 06:58:13 -0700

    alb

--- acm-certificate.tf
+++ acm-certificate.tf
@@ -1,13 +1,5 @@
 #acm module to create and verify the SSL certificate
 module "acm"{
     source  = "terraform-aws-modules/acm/aws"
-    version = "2.14.0"
-
-    
-   # domain_name  = trimsuffix(data.aws_route53_zone.mydomain.name,".")
-    #zone_id = data.aws_route53_zone.mydomain.zone_id
-  
+    version = "2.14.0"    
 }
-output "this_acm_certificate_arn" {
-      value = "module.acm.this_acm_certificate_arn"
-  }
